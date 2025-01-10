b2698f5d6eb744862218555971a239226fbc8d67
Коробов Павел Кириллович <122681088+MOCKINGBIRDem@users.noreply.github.com>
2025-01-10 20:22:34 +0500

    2 окно
    Визуализация окна с помощью WPF + логика второстепенного окна для добавления проекции детали на основной экран в рабочую область

---
+++ InputDialog.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace UnifiedApp
+{
+    public partial class InputDialog : Window
+    {
+        public double WidthInput { get; private set; }
+        public double HeightInput { get; private set; }
+
+        public InputDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(WidthBox.Text, out double width) &&
+                double.TryParse(HeightBox.Text, out double height))
+            {
+                WidthInput = width;
+                HeightInput = height;
+                DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show("Введите корректные размеры.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
